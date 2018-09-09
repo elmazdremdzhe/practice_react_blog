575ff001a320f85ec6c0b08ba3c2fd90a9e51783
elmazkhussainov <dremdzhe@gmail.com>
2018-09-09 04:14:33 -0700

    Links to individual posts

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
 
+import {connect} from 'react-redux';
+import {fetchPost }from '../actions/index';
+import { Link } from 'react-router-dom';
+
 class PostsShow extends Component{
+
+    componentDidMount(){
+        const id = this.props.match.params.id;
+        this.props.fetchPost(id);
+    }
+
     render(){
+        const {post} = this.props;
+
+        if(!post){
+            return(
+                <div>Loading...</div>
+            );
+        }
+
         return(
             <div>
-                {console.log(this.props)}
-                Post Show
+                <Link to="/">Back to Index</Link>
+                <h3>{post.title}</h3>
+                <h6>Categories: {post.categories}</h6>
+                <p>{post.content}</p>
             </div>
         );
     }
 }
 
-export default PostsShow;
+function mapStateToProps({posts}, ownProps){
+    return { post: posts[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, {fetchPost})(PostsShow);
